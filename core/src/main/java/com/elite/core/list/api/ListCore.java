0671f83c5c9e6dc1a0ef63e36f86d816586d63d2
wjc133 <wjc133@sina.com>
2016-03-23 00:29:52 +0800

    add sth

---
+++ core/src/main/java/com/elite/core/list/api/ListCore.java
@@ -0,0 +1,13 @@
+package com.elite.core.list.api;
+
+import com.elite.core.base.BaseCore;
+
+/**
+ * Created by wjc133.
+ * Date: 2016/3/22
+ * Time: 23:48
+ * Description:
+ */
+public interface ListCore extends BaseCore {
+    
+}
---
+++ core/src/main/java/com/elite/core/list/api/ListCoreImpl.java
@@ -0,0 +1,13 @@
+package com.elite.core.list.api;
+
+import com.elite.core.ApiCore;
+
+/**
+ * Created by wjc133.
+ * Date: 2016/3/22
+ * Time: 23:50
+ * Description:
+ */
+public class ListCoreImpl extends ApiCore implements ListCore {
+
+}
